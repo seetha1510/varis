99f83151b8cb4592a9a49bfca3ddf5d0c51a2dd6
davidtang332 <davidtang332@gmail.com>
2022-03-23 15:05:54 -0500

    updated corresponding files for unit testing

--- CombineDescriptions.py
+++ CombineDescriptions.py
@@ -14,40 +14,49 @@ import re
 
 # connect to sql server
 # conn = sqlite3.connect(":memory:")
-databaseName = "test2.db"
-conn = sqlite3.connect(databaseName)
-cursor = conn.cursor()
 
-try: #Combine the descriptions and store to dedicated column
-    cursor.execute("UPDATE products SET CombinedDescription = COALESCE(ItemTitle, '') || ' ' || COALESCE(ItemDescription, '') || ' ' || COALESCE(ItemBulletPoint, '') || ' ' || COALESCE(ItemDescription_2, '') || ' ' || COALESCE(ItemFactTag, '')")
-    conn.commit()
-except:
+def getConnection(dbName):
+    conn = sqlite3.connect(dbName)
+    return conn
+
+def combineDescriptions(conn):
+    cursor = conn.cursor()
+    try: #Combine the descriptions and store to dedicated column
+        cursor.execute("UPDATE products SET CombinedDescription = COALESCE(ItemTitle, '') || ' ' || COALESCE(ItemDescription, '') || ' ' || COALESCE(ItemBulletPoint, '') || ' ' || COALESCE(ItemDescription_2, '') || ' ' || COALESCE(ItemFactTag, '')")
+        conn.commit()
+    except:
+        conn.close()
+        print("Unable to build the combined descriptions column.")
+
+    """
+    try: #strip out html
+        cursor.execute("SELECT SKU, CombinedDescription FROM products")
+        description = cursor.fetchall()
+        
+        #Need to clean out HTML tags from descriptions
+        cleanD = re.compile('<.*?>')
+        def HTMLStrip(desc):
+            clean_desc = re.sub(cleanD, '', desc)
+            return clean_desc
+        
+        #Clean out each description and then reinsert it to the table column
+        i = 1
+        #cursor.execute("SET QUOTED_IDENTIFIER OFF;")
+        for d in description:
+            clean_desc = HTMLStrip(str(d[1]))
+            print("Row: " + str(i) + " : "+ clean_desc)
+            i = i+1
+            cursor.execute("UPDATE products SET CombinedDescription = '" + clean_desc + "' WHERE SKU = " + str(d[0]))
+    except:
+        conn.close()        
+    """
+
+def main():
+    conn = getConnection("test2.db")
+
+    combineDescriptions(conn)
+    #Close database connection
     conn.close()
-    print("Unable to build the combined descriptions column.")
 
-"""
-try: #strip out html
-    cursor.execute("SELECT SKU, CombinedDescription FROM products")
-    description = cursor.fetchall()
-    
-    #Need to clean out HTML tags from descriptions
-    cleanD = re.compile('<.*?>')
-    def HTMLStrip(desc):
-        clean_desc = re.sub(cleanD, '', desc)
-        return clean_desc
-    
-    #Clean out each description and then reinsert it to the table column
-    i = 1
-    #cursor.execute("SET QUOTED_IDENTIFIER OFF;")
-    for d in description:
-        clean_desc = HTMLStrip(str(d[1]))
-        print("Row: " + str(i) + " : "+ clean_desc)
-        i = i+1
-        cursor.execute("UPDATE products SET CombinedDescription = '" + clean_desc + "' WHERE SKU = " + str(d[0]))
-except:
-    conn.close()        
-"""
-
-#Close database connection
-conn.close()
-
+if __name__ == "__main__":
+    main()
--- extract_data.py
+++ extract_data.py
@@ -6,33 +6,27 @@ import pandas as pd
 import sqlite3
 
 # import data set
-dataset = pd.read_csv('Capstone_Dataset_1.csv', index_col=False)
-print("Data set read!")
+def importDataSet(data):
+    dataset = pd.read_csv(data, index_col=False)
+    print("Data set read!")
+    return dataset
 
 # connect to sql server
 # conn = sqlite3.connect(":memory:")
-databaseName = "test2.db"
-if os.path.exists(databaseName):
-    os.remove(databaseName)
-conn = sqlite3.connect("test2.db")
-cursor = conn.cursor()
+def getConnection(dbName):
+    if os.path.exists(dbName):
+        os.remove(dbName)
+    conn = sqlite3.connect(dbName)
+    return conn
 
+def main():
+    dataset = importDataSet('Capstone_Dataset_1.csv')
+    conn = getConnection('test2.db')
+    cursor = conn.cursor()
 
-# create tables
+    # create tables
 
-try: #Builds segments table
-    segment_table = '''
-    CREATE TABLE segments(
-        SEG_ID text PRIMARY KEY,
-        SEG_NAME text
-    )
-    '''
-    cursor.execute(segment_table)
-    print("Segment table created")
-except:
-    
-    try:#delete existing table and remake it
-        cursor.execute("DROP TABLE IF EXISTS segments")
+    try: #Builds segments table
         segment_table = '''
         CREATE TABLE segments(
             SEG_ID text PRIMARY KEY,
@@ -42,27 +36,25 @@ except:
         cursor.execute(segment_table)
         print("Segment table created")
     except:
-        conn.close()
-        print("Unable to create segment table, closing connection.")
+        
+        try:#delete existing table and remake it
+            cursor.execute("DROP TABLE IF EXISTS segments")
+            segment_table = '''
+            CREATE TABLE segments(
+                SEG_ID text PRIMARY KEY,
+                SEG_NAME text
+            )
+            '''
+            cursor.execute(segment_table)
+            print("Segment table created")
+        except:
+            conn.close()
+            print("Unable to create segment table, closing connection.")
 
-try: #Builds categories table
-    category_table = '''
-    CREATE TABLE categories(
-        CAT_ID text PRIMARY KEY ,
-        CAT_NAME text,
-        SEG_ID text NOT NULL,
-        FOREIGN KEY(SEG_ID) REFERENCES segments(SEG_ID)
-    )
-    '''
-    cursor.execute(category_table)
-    print("Category table created")
-except:
-    
-    try:#delete existing table and remake it
-        cursor.execute("DROP TABLE IF EXISTS categories")
+    try: #Builds categories table
         category_table = '''
         CREATE TABLE categories(
-            CAT_ID text PRIMARY KEY,
+            CAT_ID text PRIMARY KEY ,
             CAT_NAME text,
             SEG_ID text NOT NULL,
             FOREIGN KEY(SEG_ID) REFERENCES segments(SEG_ID)
@@ -71,24 +63,24 @@ except:
         cursor.execute(category_table)
         print("Category table created")
     except:
-        conn.close()
-        print("Unable to create category table, closing connection.")
         
-try: #Builds classes table
-    class_table = '''
-    CREATE TABLE classes(
-        CLS_ID text PRIMARY KEY,
-        CLS_NAME text,
-        CAT_ID text NOT NULL,
-        FOREIGN KEY(CAT_ID) REFERENCES categories(CAT_ID)
-    )
-    '''
-    cursor.execute(class_table)
-    print("Class table created")
-except:
-    
-    try:#delete existing table and remake it
-        cursor.execute("DROP TABLE IF EXISTS classes")
+        try:#delete existing table and remake it
+            cursor.execute("DROP TABLE IF EXISTS categories")
+            category_table = '''
+            CREATE TABLE categories(
+                CAT_ID text PRIMARY KEY,
+                CAT_NAME text,
+                SEG_ID text NOT NULL,
+                FOREIGN KEY(SEG_ID) REFERENCES segments(SEG_ID)
+            )
+            '''
+            cursor.execute(category_table)
+            print("Category table created")
+        except:
+            conn.close()
+            print("Unable to create category table, closing connection.")
+            
+    try: #Builds classes table
         class_table = '''
         CREATE TABLE classes(
             CLS_ID text PRIMARY KEY,
@@ -100,24 +92,24 @@ except:
         cursor.execute(class_table)
         print("Class table created")
     except:
-        conn.close()
-        print("Unable to create class table, closing connection.")
+        
+        try:#delete existing table and remake it
+            cursor.execute("DROP TABLE IF EXISTS classes")
+            class_table = '''
+            CREATE TABLE classes(
+                CLS_ID text PRIMARY KEY,
+                CLS_NAME text,
+                CAT_ID text NOT NULL,
+                FOREIGN KEY(CAT_ID) REFERENCES categories(CAT_ID)
+            )
+            '''
+            cursor.execute(class_table)
+            print("Class table created")
+        except:
+            conn.close()
+            print("Unable to create class table, closing connection.")
 
-try: #Builds subclasses table
-    subclass_table = '''
-    CREATE TABLE subclasses(
-        SUB_ID text PRIMARY KEY,
-        SUB_NAME text,
-        CLS_ID text NOT NULL,
-        FOREIGN KEY(CLS_ID) REFERENCES classes(CLS_ID)
-    )
-    '''
-    cursor.execute(subclass_table)
-    print("SubClass table created")
-except:
-    
-    try:#delete existing table and remake it
-        cursor.execute("DROP TABLE IF EXISTS subclasses")
+    try: #Builds subclasses table
         subclass_table = '''
         CREATE TABLE subclasses(
             SUB_ID text PRIMARY KEY,
@@ -129,42 +121,24 @@ except:
         cursor.execute(subclass_table)
         print("SubClass table created")
     except:
-        conn.close()
-        print("Unable to create SubClass table, closing connection.")
+        
+        try:#delete existing table and remake it
+            cursor.execute("DROP TABLE IF EXISTS subclasses")
+            subclass_table = '''
+            CREATE TABLE subclasses(
+                SUB_ID text PRIMARY KEY,
+                SUB_NAME text,
+                CLS_ID text NOT NULL,
+                FOREIGN KEY(CLS_ID) REFERENCES classes(CLS_ID)
+            )
+            '''
+            cursor.execute(subclass_table)
+            print("SubClass table created")
+        except:
+            conn.close()
+            print("Unable to create SubClass table, closing connection.")
 
-try: #Builds product table
-    product_table = '''
-    CREATE TABLE products(
-        SKU int PRIMARY KEY,
-        ItemTitle text,
-        ItemDescription text,
-        ItemBulletPoint text,
-        ItemDescription_2 text,
-        Manufacturer text,
-        MfrPartNum text,
-        SellUOM text,
-        ItemPrice float,
-        ItemFactTag text,
-        Segment text,
-        SegmentName text,
-        Category text,
-        CategoryName text,
-        Class text, 
-        ClassName text,
-        SubClass text,
-        SubClassName text,
-        CombinedDescription text,
-        FOREIGN KEY(Segment) REFERENCES segments(SEG_ID),
-        FOREIGN KEY(Category) REFERENCES categories(CAT_ID),
-        FOREIGN KEY(Class) REFERENCES classes(CLS_ID),
-        FOREIGN KEY(SubClass) REFERENCES subclasses(SUB_ID)
-    )
-    '''
-    cursor.execute(product_table)
-    print("Products table created")
-except:
-    try: #delete existing table and remake it
-        cursor.execute("DROP TABLE IF EXISTS products")
+    try: #Builds product table
         product_table = '''
         CREATE TABLE products(
             SKU int PRIMARY KEY,
@@ -195,53 +169,88 @@ except:
         cursor.execute(product_table)
         print("Products table created")
     except:
-        conn.close()
-        print("Unable to create products table, closing connection.")
+        try: #delete existing table and remake it
+            cursor.execute("DROP TABLE IF EXISTS products")
+            product_table = '''
+            CREATE TABLE products(
+                SKU int PRIMARY KEY,
+                ItemTitle text,
+                ItemDescription text,
+                ItemBulletPoint text,
+                ItemDescription_2 text,
+                Manufacturer text,
+                MfrPartNum text,
+                SellUOM text,
+                ItemPrice float,
+                ItemFactTag text,
+                Segment text,
+                SegmentName text,
+                Category text,
+                CategoryName text,
+                Class text, 
+                ClassName text,
+                SubClass text,
+                SubClassName text,
+                CombinedDescription text,
+                FOREIGN KEY(Segment) REFERENCES segments(SEG_ID),
+                FOREIGN KEY(Category) REFERENCES categories(CAT_ID),
+                FOREIGN KEY(Class) REFERENCES classes(CLS_ID),
+                FOREIGN KEY(SubClass) REFERENCES subclasses(SUB_ID)
+            )
+            '''
+            cursor.execute(product_table)
+            print("Products table created")
+        except:
+            conn.close()
+            print("Unable to create products table, closing connection.")
 
-for row in dataset.itertuples():
-    cursor.execute('''
-        INSERT OR IGNORE INTO segments (SEG_ID, SEG_NAME)
-        VALUES (?, ?)
-        ''',
-        tuple(row)[11:13]
-    )
-    
-    cursor.execute('''
-        INSERT OR IGNORE INTO categories (CAT_ID, CAT_NAME, SEG_ID)
-        VALUES (?, ?, ?)
-        ''',
-        tuple((row[13], row[14], row[11]))
-    )
-    
-    cursor.execute('''
-        INSERT OR IGNORE INTO classes (CLS_ID, CLS_NAME, CAT_ID)
-        VALUES (?, ?, ?)
-        ''',
-        tuple((row[15],row[16],row[13]))
-    )
-    
-    cursor.execute('''
-        INSERT OR IGNORE INTO subclasses (SUB_ID, SUB_NAME, CLS_ID)
-        VALUES (?, ?, ?)
-        ''',
-        tuple((row[17],row[18],row[15]))
-    )
-    
-    cursor.execute('''
-        INSERT INTO products (SKU, ItemTitle, ItemDescription, ItemBulletPoint, ItemDescription_2, Manufacturer, MfrPartNum, SellUOM, ItemPrice, ItemFactTag, Segment, SegmentName, Category, CategoryName, Class, ClassName, SubClass, SubClassName)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''',
-        tuple(row)[1:]
-    )
-conn.commit()
+    for row in dataset.itertuples():
+        cursor.execute('''
+            INSERT OR IGNORE INTO segments (SEG_ID, SEG_NAME)
+            VALUES (?, ?)
+            ''',
+            tuple(row)[11:13]
+        )
+        
+        cursor.execute('''
+            INSERT OR IGNORE INTO categories (CAT_ID, CAT_NAME, SEG_ID)
+            VALUES (?, ?, ?)
+            ''',
+            tuple((row[13], row[14], row[11]))
+        )
+        
+        cursor.execute('''
+            INSERT OR IGNORE INTO classes (CLS_ID, CLS_NAME, CAT_ID)
+            VALUES (?, ?, ?)
+            ''',
+            tuple((row[15],row[16],row[13]))
+        )
+        
+        cursor.execute('''
+            INSERT OR IGNORE INTO subclasses (SUB_ID, SUB_NAME, CLS_ID)
+            VALUES (?, ?, ?)
+            ''',
+            tuple((row[17],row[18],row[15]))
+        )
+        
+        cursor.execute('''
+            INSERT INTO products (SKU, ItemTitle, ItemDescription, ItemBulletPoint, ItemDescription_2, Manufacturer, MfrPartNum, SellUOM, ItemPrice, ItemFactTag, Segment, SegmentName, Category, CategoryName, Class, ClassName, SubClass, SubClassName)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''',
+            tuple(row)[1:]
+        )
+    conn.commit()
 
-# handle missing data
+    # handle missing data
 
-# handle categorical data
+    # handle categorical data
 
-# split data into training and testing sets
+    # split data into training and testing sets
 
-# feature scale
+    # feature scale
 
-#Close connection
-conn.close()
+    #Close connection
+    conn.close()
+
+if __name__ == "__main__":
+    main()
